4ed7f7a26cd9a7647c655d264e28c23464ad7574
Si Pham <phamtrisi@gmail.com>
2017-12-06 02:02:02 -0800

    edit date

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const backfill645Results = require('./utils/logic/backfill645Results');
 
 function scrape() {
     console.log('Init cronjob');
-    backfill645Results(new Date('2016-01-01'), new Date('2017-12-07'));
+    backfill645Results(new Date('2017-07-01'), new Date('2017-12-07'));
     //vietlott.runScrapers();
     // try {
     //     var job = new CronJob({
