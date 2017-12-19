984b177560c6b06c22fc7576038eed7812283476
Si Pham <phamtrisi@gmail.com>
2017-12-18 21:36:32 -0800

    fixed stray code

--- utils/scrapers/index.js
+++ utils/scrapers/index.js
@@ -1,5 +1,6 @@
 const max4DScraper = require('./max4D');
 const vietlott645 = require('./vietlott645');
+const vietlott655 = require('./vietlott655');
 
 module.exports = {
     max4D: max4DScraper,
